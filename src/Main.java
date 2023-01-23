a4a3ab4d208ab14216e466a4fa657b43d615b581
Aleksey <alek7mak@gmail.com>
2023-01-23 12:49:30 +0300

    Task 2.10

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,84 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String[] array = sc.nextLine().split(" ");
+class MyProgram {
+    static public void main (String []args) throws FileNotFoundException {
+        String path = "src/test/input.txt";
+
+        File file = new File(path);
+        Scanner sc = new Scanner(file);
+        String line = sc.nextLine();
         sc.close();
 
-        if (Double.parseDouble(array[2]) == 0.0) {
-            System.out.println("Error! Division by zero");
-        } else {
-            try {
-                switch (array[1]) {
-                    case "+":
-                        System.out.println(Double.parseDouble(array[0]) + Double.parseDouble(array[2]));
-                        break;
-                    case "-":
-                        System.out.println(Double.parseDouble(array[0]) - Double.parseDouble(array[2]));
-                        break;
-                    case "*":
-                        System.out.println(Double.parseDouble(array[0]) * Double.parseDouble(array[2]));
-                        break;
-                    case "/":
-                        System.out.println(Double.parseDouble(array[0]) / Double.parseDouble(array[2]));
-                        break;
+        double firstNum = 0.0;
+        double secondNum = 0.0;
+        char sign = ' ';
+        int i = 0;
+        boolean is = true;
+
+        for(String num:line.split(" ")) {
+            i++;
+            if(i == 1) {
+                try {
+                    firstNum = Double.parseDouble(num);
+                }catch(NumberFormatException e) {
+                    System.out.print("Error! Not number\n");
+                    is = false;
+                    break;
                 }
-            } catch (NumberFormatException e) {
-                System.out.println("Error! Not number\n");
-            } catch (Exception e) {
-                System.out.println("Operation error!");
+            }
+            if(i == 3) {
+                try {
+                    secondNum = Double.parseDouble(num);
+                }catch(NumberFormatException e) {
+                    System.out.print("Error! Not number\n");
+                    is = false;
+                    break;
+                }
             }
         }
+        if(is) {
+            i = 0;
+            for(String num:line.split(" ")) {
+                i++;
+                if(i == 2) {
+                    try {
+                        sign = result(num);
+                    }catch(Exception s){
+                        System.out.print("Operation Error!");
+                        is = false;
+                        break;
+                    }
+                }
+            }
+        }
+        if(is) {
+            switch(sign){
+                case('+'):
+                    System.out.print(firstNum + secondNum);
+                    break;
+                case('-'):
+                    System.out.print(firstNum - secondNum);
+                    break;
+                case('*'):
+                    System.out.print(firstNum * secondNum);
+                    break;
+                case('/'):
+                    if(secondNum == 0.0) {
+                        System.out.print("Error! Division by zero");
+                    }else {
+                        System.out.print(firstNum / secondNum);
+                    }
+                    break;
+            }
+        }
+    }
+
+    public static char result(String nms) throws Exception {
+        if(nms.equals("/") || nms.equals("*") || nms.equals("+") || nms.equals("-")) {
+            return nms.charAt(0);
+        }
+        else throw new Exception("OperationError");
     }
 }
