52f5a9cfe801e4b4ffa9103348ad46189ecafa83
Aleksey <alek7mak@gmail.com>
2023-02-08 18:14:45 +0300

    hashCode & equals

--- src/Compilation/Main.java
+++ src/Compilation/Main.java
@@ -35,8 +35,28 @@ class Main {
             return im;
         }
 
+        @Override
+        public int hashCode() {
+            int result;
+            long temp;
 
+            temp = Double.doubleToLongBits(re);
+            result = (int) (temp - (temp >>> 32));
+            temp = Double.doubleToLongBits(im);
+            result = 37 * result + (int) (temp - (temp >>> 32));
+            return result;
+        }
 
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            ComplexNumber that = (ComplexNumber) o;
+
+            if (Double.compare(that.re, re) != 0) return false;
+            return Double.compare(that.im, im) == 0;
+        }
     }
 
 
