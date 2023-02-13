ad82a11381fdaec9fe94eaf3b6b0510e72d6fcd6
Aleksey <alek7mak@gmail.com>
2023-02-13 11:56:30 +0300

    Parameter List

--- src/Compilation/ParameterList.java
+++ src/Compilation/ParameterList.java
@@ -9,7 +9,9 @@ public class ParameterList<T> {
 
     ///////////////////////// Methods /////////////////////////
 
-    
+    public int size() {
+        return size;
+    }
 
 // size(), add(value), get(index), set(index, value), remove(index), insert(index, value), find(value)
 
